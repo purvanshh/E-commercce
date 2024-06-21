1bac6cbcbc1286f6904e7edba46a2d7ab5f97b2a
Purvansh <purvansh.23bcs10169@ms.sst.scaler.com>
2024-06-21 09:19:15 +0530

    Add to cart added

--- src/App.js
+++ src/App.js
@@ -1,37 +1,54 @@
-import React, { useState } from 'react';
 import './App.css';
-import Products, { a, b } from './components/Products/Products';
-
+import { a, b } from './components/Products/Products';
+import Products from './components/Products/Products';
+import { useState } from 'react';
 function App() {
-  const [card, setCard] = useState({
-    quantity: 0
-  });
+  // state variable
+  // inc
+  // dec
+  let [cart, setCart] = useState({});
+  function increaseQuantity(product) {
+    const newCart = { ...cart };
+    // if(cart[product.id])
+    if (!newCart[product.id]) {
+      newCart[product.id] = {
+        ...product,
+        quantity: 0
+      };
+    }
+    newCart[product.id].quantity += 1;
+    console.log(newCart);
+    setCart(newCart);
+  }
 
-  const increaseQuantity = () => {
-    setCard(prevCard => ({
-      ...prevCard,
-      quantity: prevCard.quantity + 1
-    }));
-  };
-
-  const decreaseQuantity = () => {
-    setCard(prevCard => ({
-      ...prevCard,
-      quantity: Math.max(prevCard.quantity - 1, 0) // Prevent negative quantity
-    }));
-  };
+  function decreaseQuantity(product) {
+    const newCart = { ...cart };
+    if (!newCart[product.id]) return;
+    newCart[product.id].quantity -= 1;
+    if (newCart[product.id].quantity <= 0) {
+      delete newCart[product.id];
+    }
+    setCart(newCart);
+  }
 
   console.log(a, b);
-  
   return (
     <div className="App">
-      <Products 
-        card={card} 
-        increaseQuantity={increaseQuantity} 
-        decreaseQuantity={decreaseQuantity} 
-      />
+      <Products cart={cart} increaseQuantity={increaseQuantity} decreaseQuantity={decreaseQuantity} />
     </div>
   );
 }
 
 export default App;
+
+// {1:{id: 1, quantity: 11}, 2:{id: 2, quantity: 10}, 3:{id: 3, quantity: 10}, 4:{id: 4, quantity: 10}}
+
+// button
+// - 1 +
+
+// let a = {b:10, c:20};
+//a.b = 30;
+// let b = {...a};
+
+// a => 1234
+// a => 12
